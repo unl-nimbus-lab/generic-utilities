1a05beac65e82352bc503d18ba4dd0824aa775c6
aj <aj.nimbus.edu>
2019-11-08 10:39:41 -0600

    Add methods for manually setting up static IP

--- share_internet.sh
+++ share_internet.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
 # Non-persistent internet forwarding for Linux. Uses sudo.
+# Run this on **HOST COMPUTER ONLY**.
 # NOTE:
 #   -o $1 (eth0)   :   This is your computer's internet connection
 #   -i $2 (eth1)   :   This is what you want to share internet to
@@ -31,7 +32,19 @@ sudo iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 echo "Done! Check /etc/sysctl.conf for net.ip_forward if needed.."
 
-# following lines go on the odroid/pi/other-computer
-# sudo route add default gw 20.0.0.2 eth-whatever
-# echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
-
+## Two styles for odroid/pi/other-computer:
+#    1. Non-persistent settings:
+#         sudo route add default gw <host-computer-ip> <odroid-eth-whatever>
+#         echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+#    2. Automatic persistent network interface:
+#       Edit the file /etc/network/interfaces to add these lines:
+#         auto eth0
+#         iface eth0 inet static
+#         address abc.def.ghi.JKL                       # static ip for odroid/pi
+#         network abc.0.0.0
+#         broadcast abc.0.0.255
+#         netmask 255.255.255.0
+#         up route add default gw abc.def.ghi.XYZ       # host computer's ip (note XYZ)
+#         dns-nameservers 8.8.8.8
+#       The second-last line works beautifully if you are on Ubuntu16+. Not tested on
+#       previous versions. Simple addr. are easier (50.0.0.1 for host, 50.0.0.2 for odroid/pi).
